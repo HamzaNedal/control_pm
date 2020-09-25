b0e78c7b970dcc47607317c3c177c44c02cffec5
omar al haddad <42090545+omarmhd@users.noreply.github.com>
2020-09-26 00:40:15 +0300

    3 commit omar

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,9 +19,27 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            //client
+            $table->string('Phone')->nullable();
+            $table->string('Payment')->nullable();
+            $table->integer('words')->nullable();
+
+            //provider
+            $table->string('payment_method')->nullable();
+             $table->string('education_level')->nullable();
+             $table->string('name_university')->nullable();
+             $table->integer('years_experience')->nullable();
+             $table->integer('capacity_day')->nullable();
+             $table->string('subjects')->nullable();
+             $table->string('country')->nullable();
+             $table->string('Whats_up')->nullable();
+
+
+
             $table->rememberToken();
             $table->timestamps();
         });
+
     }
 
     /**
